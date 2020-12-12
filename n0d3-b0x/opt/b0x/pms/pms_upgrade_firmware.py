0f508768bb070b929cf4a096f8faa06fc734a8e1
AD <ad.pub@pm.me>
2020-12-12 20:54:10 +0100

    -

--- n0d3-b0x/opt/b0x/pms/pms_upgrade_firmware.py
+++ n0d3-b0x/opt/b0x/pms/pms_upgrade_firmware.py
@@ -6,13 +6,16 @@ import sys, os
 
 api = SixfabPower()
 
-firmware_path = "./firmwares/sixfab_pms_firmware_v0.3.0.bin"
+firmware_path = "/opt/b0x/sixfab_pms_firmware_v0.3.3.bin"
 
 error_occured = 0
 try:
+    api.reset_mcu()
+    api.reset_for_boot_update()
+    time.sleep(5)
     for step in api.update_firmware(firmware_path):
         print(f"{step}%")
-        
+
 except:
     error_occured = 1
     print("raised error")
@@ -21,7 +24,7 @@ finally:
     update_successful = 0
     if error_occured is 0:
         counter = 0
-        
+
         for i in range(15):
             print('.', sep=' ', end=' ', flush=True)
             time.sleep(1)
@@ -40,9 +43,9 @@ finally:
                 if update_successful is 1:
                     print("\nUpdate is Successful to FW Ver: " + str(fw_ver))
                     break
-                else: 
+                else:
                     if counter >= 10:
                         print("Update is Failed!")
                         break
-            
+
             time.sleep(2)
